8eb8eca9444ee8876d3d5e8c38898914770d3aed
imagine1A <nathanieladdoquaye15@gmail.com>
2023-03-11 18:19:30 +0000

    Initial commit

---
+++ assets/js/aos.js
@@ -0,0 +1,5 @@
+AOS.init({
+    offset:200,
+    duration: 700,
+    easing:"ease-in-out"
+});
---
+++ assets/js/zscript.js
@@ -0,0 +1,771 @@
+const mySlider_nav = new Swiper('.mySlider-nav', {
+    // Optional parameters
+    direction: 'horizontal',
+   
+    // width: 320,
+    // slideOffsetAfter: -60,
+    // swiperSlideSize: 320,
+    
+    slidesPerGroup:2,
+    slidesPerView : 6,
+    spaceBetween : -100, 
+    
+    breakpoints: {
+        360: {
+    slidesPerGroup:1,
+    slidesPerView : 1,
+    spaceBetween : -190, 
+        },
+        
+        576: {
+    slidesPerGroup:2,
+    slidesPerView : 3,
+    spaceBetween : -40, 
+        },
+        
+         768: {
+    slidesPerGroup:2,
+    slidesPerView : 4,
+    spaceBetween : -100, 
+        },
+        992: {
+    slidesPerGroup:2,
+    slidesPerView : 4,
+    spaceBetween : -100, 
+        },
+        
+        1200: {
+    slidesPerGroup:2,
+    slidesPerView : 6,
+    spaceBetween : -100, 
+        }
+    },
+   
+  });
+
+
+
+const swiper1 = new Swiper('.mySlider-1', {
+    // Optional parameters
+    direction: 'horizontal',
+   
+    // width: 320,
+    slideOffsetAfter: -60,
+    // swiperSlideSize: 320,
+    
+    slidesPerGroup:2,
+    slidesPerView : 1,
+    spaceBetween : -150, 
+    
+    breakpoints: {
+        360: {
+    slidesPerGroup:1,
+    slidesPerView : 1,
+    spaceBetween : 190, 
+        },
+        
+        576: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : 190, 
+        },
+        
+         768: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : 20, 
+        },
+        992: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : -150, 
+        },
+        
+        1200: {
+    slidesPerGroup:2,
+    slidesPerView : 3,
+    spaceBetween : -150, 
+        }
+    },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination1',
+     type: 'progressbar',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next1',
+      prevEl: '.swiper-button-prev1',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
+
+
+const swiper2 = new Swiper('.mySlider-2', {
+    // Optional parameters
+    direction: 'horizontal',
+   
+    
+     slideOffsetAfter: -60,
+    // swiperSlideSize: 320,
+    slidesPerGroup:2,
+    slidesPerView : 3,
+    spaceBetween : -150,
+    
+    breakpoints: {
+        360: {
+    slidesPerGroup:1,
+    slidesPerView : 1,
+    spaceBetween : 190, 
+        },
+        
+        576: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : 190, 
+        },
+        
+         768: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : 20, 
+        },
+        992: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : -150, 
+        },
+        
+        1200: {
+    slidesPerGroup:2,
+    slidesPerView : 3,
+    spaceBetween : -150, 
+        }
+    },
+    
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination2',
+     type: 'progressbar',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next2',
+      prevEl: '.swiper-button-prev2',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
+
+
+
+const swiper3 = new Swiper('.mySlider-3', {
+    // Optional parameters
+    direction: 'horizontal',
+   
+    
+     slideOffsetAfter: -60,
+    // swiperSlideSize: 320,
+    slidesPerGroup:2,
+    slidesPerView : 3,
+    spaceBetween : -150,
+    
+    breakpoints: {
+        360: {
+    slidesPerGroup:1,
+    slidesPerView : 1,
+    spaceBetween : 190, 
+        },
+        
+        576: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : 190, 
+        },
+        
+         768: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : 20, 
+        },
+        992: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : -150, 
+        },
+        
+        1200: {
+    slidesPerGroup:2,
+    slidesPerView : 3,
+    spaceBetween : -150, 
+        }
+    },
+    
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination3',
+     type: 'progressbar',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next3',
+      prevEl: '.swiper-button-prev3',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
+
+
+const swiper4 = new Swiper('.mySlider-4', {
+    // Optional parameters
+    direction: 'horizontal',
+   
+    
+     slideOffsetAfter: -60,
+    // swiperSlideSize: 320,
+    slidesPerGroup:1,
+    slidesPerView : 1,
+    spaceBetween : -150,
+    
+    breakpoints: {
+        360: {
+    slidesPerGroup:1,
+    slidesPerView : 1,
+    spaceBetween : 190, 
+        },
+        
+        576: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : 190, 
+        },
+        
+         768: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : 20, 
+        },
+        992: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : -150, 
+        },
+        
+        1200: {
+    slidesPerGroup:2,
+    slidesPerView : 3,
+    spaceBetween : -150, 
+        }
+    },
+    
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination4',
+     type: 'progressbar',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next4',
+      prevEl: '.swiper-button-prev4',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
+
+
+const swiper5 = new Swiper('.mySlider-5', {
+    // Optional parameters
+    direction: 'horizontal',
+   
+    
+    slideOffsetAfter: -60,
+    // swiperSlideSize: 320,
+    slidesPerGroup:1,
+    slidesPerView : 1,
+    spaceBetween : -150,
+    
+    breakpoints: {
+        360: {
+    slidesPerGroup:1,
+    slidesPerView : 1,
+    spaceBetween : 190, 
+        },
+        
+        576: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : 190, 
+        },
+        
+         768: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : 20, 
+        },
+        992: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : -150, 
+        },
+        
+        1200: {
+    slidesPerGroup:2,
+    slidesPerView : 3,
+    spaceBetween : -150, 
+        }
+    },
+    
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination5',
+     type: 'progressbar',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next5',
+      prevEl: '.swiper-button-prev5',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
+
+  const swiper6 = new Swiper('.mySlider-6', {
+    // Optional parameters
+    direction: 'horizontal',
+   
+    // width: 320,
+    slideOffsetAfter: -60,
+    // swiperSlideSize: 320,
+    
+    slidesPerGroup:2,
+    slidesPerView : 1,
+    spaceBetween : -150, 
+    
+    breakpoints: {
+        360: {
+    slidesPerGroup:1,
+    slidesPerView : 1,
+    spaceBetween : 190, 
+        },
+        
+        576: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : 190, 
+        },
+        
+         768: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : 20, 
+        },
+        992: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : -150, 
+        },
+        
+        1200: {
+    slidesPerGroup:2,
+    slidesPerView : 3,
+    spaceBetween : -150, 
+        }
+    },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination6',
+     type: 'progressbar',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next6',
+      prevEl: '.swiper-button-prev6',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
+
+  const swiper7 = new Swiper('.mySlider-7', {
+    // Optional parameters
+    direction: 'horizontal',
+   
+    // width: 320,
+    slideOffsetAfter: -60,
+    // swiperSlideSize: 320,
+    
+    slidesPerGroup:2,
+    slidesPerView : 1,
+    spaceBetween : -150, 
+    
+    breakpoints: {
+        360: {
+    slidesPerGroup:1,
+    slidesPerView : 1,
+    spaceBetween : 190, 
+        },
+        
+        576: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : 190, 
+        },
+        
+         768: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : 20, 
+        },
+        992: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : -150, 
+        },
+        
+        1200: {
+    slidesPerGroup:2,
+    slidesPerView : 3,
+    spaceBetween : -150, 
+        }
+    },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination7',
+     type: 'progressbar',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next7',
+      prevEl: '.swiper-button-prev7',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
+
+  const swiper8 = new Swiper('.mySlider-8', {
+    // Optional parameters
+    direction: 'horizontal',
+   
+    // width: 320,
+    slideOffsetAfter: -60,
+    // swiperSlideSize: 320,
+    
+    slidesPerGroup:2,
+    slidesPerView : 1,
+    spaceBetween : -150, 
+    
+    breakpoints: {
+        360: {
+    slidesPerGroup:1,
+    slidesPerView : 1,
+    spaceBetween : 190, 
+        },
+        
+        576: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : 190, 
+        },
+        
+         768: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : 20, 
+        },
+        992: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : -150, 
+        },
+        
+        1200: {
+    slidesPerGroup:2,
+    slidesPerView : 3,
+    spaceBetween : -150, 
+        }
+    },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination8',
+     type: 'progressbar',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next8',
+      prevEl: '.swiper-button-prev8',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
+
+  const swiper9 = new Swiper('.mySlider-9', {
+    // Optional parameters
+    direction: 'horizontal',
+   
+    // width: 320,
+    slideOffsetAfter: -60,
+    // swiperSlideSize: 320,
+    
+    slidesPerGroup:2,
+    slidesPerView : 1,
+    spaceBetween : -150, 
+    
+    breakpoints: {
+        360: {
+    slidesPerGroup:1,
+    slidesPerView : 1,
+    spaceBetween : 190, 
+        },
+        
+        576: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : 190, 
+        },
+        
+         768: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : 20, 
+        },
+        992: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : -150, 
+        },
+        
+        1200: {
+    slidesPerGroup:2,
+    slidesPerView : 3,
+    spaceBetween : -150, 
+        }
+    },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination9',
+     type: 'progressbar',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next9',
+      prevEl: '.swiper-button-prev9',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
+
+  const swiper10 = new Swiper('.mySlider-10', {
+    // Optional parameters
+    direction: 'horizontal',
+   
+    // width: 320,
+    slideOffsetAfter: -60,
+    // swiperSlideSize: 320,
+    
+    slidesPerGroup:2,
+    slidesPerView : 1,
+    spaceBetween : -150, 
+    
+    breakpoints: {
+        360: {
+    slidesPerGroup:1,
+    slidesPerView : 1,
+    spaceBetween : 190, 
+        },
+        
+        576: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : 190, 
+        },
+        
+         768: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : 20, 
+        },
+        992: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : -150, 
+        },
+        
+        1200: {
+    slidesPerGroup:2,
+    slidesPerView : 3,
+    spaceBetween : -150, 
+        }
+    },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination10',
+     type: 'progressbar',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next10',
+      prevEl: '.swiper-button-prev10',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
+
+  const swiper11 = new Swiper('.mySlider-11', {
+    // Optional parameters
+    direction: 'horizontal',
+   
+    // width: 320,
+    slideOffsetAfter: -60,
+    // swiperSlideSize: 320,
+    
+    slidesPerGroup:2,
+    slidesPerView : 1,
+    spaceBetween : -150, 
+    
+    breakpoints: {
+        360: {
+    slidesPerGroup:1,
+    slidesPerView : 1,
+    spaceBetween : 190, 
+        },
+        
+        576: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : 190, 
+        },
+        
+         768: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : 20, 
+        },
+        992: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : -150, 
+        },
+        
+        1200: {
+    slidesPerGroup:2,
+    slidesPerView : 3,
+    spaceBetween : -150, 
+        }
+    },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination11',
+     type: 'progressbar',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next11',
+      prevEl: '.swiper-button-prev11',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
+
+const swiper12 = new Swiper('.mySlider-12', {
+    // Optional parameters
+    direction: 'horizontal',
+   
+    
+    slideOffsetAfter: -60,
+    // swiperSlideSize: 320,
+    slidesPerGroup:2,
+    slidesPerView : 3,
+    spaceBetween : -190,
+    
+    breakpoints: {
+        360: {
+    slidesPerGroup:1,
+    slidesPerView : 1,
+    spaceBetween : 190, 
+        },
+        
+        576: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : 190, 
+        },
+        
+         768: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : 20, 
+        },
+        992: {
+    slidesPerGroup:2,
+    slidesPerView : 2,
+    spaceBetween : -150, 
+        },
+        
+        1200: {
+    slidesPerGroup:2,
+    slidesPerView : 3,
+    spaceBetween : -150, 
+        }
+    },
+    
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination12',
+     type: 'progressbar',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next12',
+      prevEl: '.swiper-button-prev12',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
